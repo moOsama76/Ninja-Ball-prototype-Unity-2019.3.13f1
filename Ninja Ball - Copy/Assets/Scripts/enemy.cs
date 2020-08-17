6b1556ade6487d0db8ba99c26e280777df41b411
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2020-08-17 16:13:21 +0200

    Add files via upload

---
+++ Ninja Ball - Copy/Assets/Scripts/enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+    float speed = 3f;
+    void FixedUpdate()
+    {
+        if(GetComponent<Rigidbody2D>().angularVelocity < 200){
+            GetComponent<Rigidbody2D>().AddTorque(speed);
+        }
+    }
+}
