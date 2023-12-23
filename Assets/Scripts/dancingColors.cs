bf5a9eb74bcd866b4c852dee36806022d096e63b
Mohamed Osama <67656249+moOsama76@users.noreply.github.com>
2023-12-23 12:20:24 +0200

    adding missing files

---
+++ Assets/Scripts/dancingColors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+
+public class dancingColors : MonoBehaviour
+{
+     SpriteRenderer clr;
+     public float[] RGB;
+     void Start()
+    {
+        StartCoroutine(changeColor());
+    }
+    
+    IEnumerator changeColor(){
+        yield return new WaitForSeconds(0.25f);
+        RGB[0] = Random.Range(0, 255);
+        RGB[1] = Random.Range(0, 255);
+        RGB[2] = Random.Range(0, 255);
+        clr = gameObject.GetComponent<SpriteRenderer>();
+        clr.GetComponent<SpriteRenderer>().color = new Color(RGB[0]/255, RGB[1]/255, RGB[2]/255, 1f);
+        StartCoroutine(changeColor());
+    }
+
+
+}
